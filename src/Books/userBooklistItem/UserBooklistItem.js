a73dc2dd75c30baa079b0a203a0f88f07a339b8e
martinr-max <74423661+martinr-max@users.noreply.github.com>
2020-11-25 20:47:44 +0200

    Update UserBooklistItem.js

--- src/Books/userBooklistItem/UserBooklistItem.js
+++ src/Books/userBooklistItem/UserBooklistItem.js
@@ -17,33 +17,33 @@ export default function UserBooklistItem({
     }) {    
 
     return(
-        <div className="container">
-	    <div className="card-wrap">
-		<Card className="root">
-			<CardActionArea>
-				<CardMedia className="userCardMedia" image={imageUrl} />
-				<CardContent>
-					<h3 className="card-title"> {title} </h3>
-					<h4 className="card-author"> {author} </h4>
-					<CardActionArea>
-						<CardActions>
-							<Button className="removeBookButton" onClick={(e)=> handleclickOpen(id)}
-								color="primary">
-								remove book
-							</Button>
-						</CardActions>
-					</CardActionArea>
-				</CardContent>
-			</CardActionArea>
-		</Card>
-		<DeleteUSerBookDialog
-		 open={open}
-		 handleClose={handleClose}
-		 handleclickOpen={handleclickOpen}
-		 handleDeleteUserBook={handleDeleteUserBook}
-		 bookid={bookIndex} />
-	    </div>
-    	</div>
+      <div className="container">
+  	<div className="card-wrap">
+    	 <Card className="root">
+      	  <CardActionArea>
+           <CardMedia className="userCardMedia" image={imageUrl} />
+            <CardContent>
+             <h3 className="card-title"> {title} </h3>
+             <h4 className="card-author"> {author} </h4>
+             <CardActionArea>
+             <CardActions>
+              <Button className="removeBookButton" onClick={(e)=> handleclickOpen(id)}
+                color="primary">
+                remove book
+              </Button>
+            </CardActions>
+          </CardActionArea>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+    <DeleteUSerBookDialog
+	open={open}
+	handleClose={handleClose}
+	handleclickOpen={handleclickOpen}
+	handleDeleteUserBook={handleDeleteUserBook}
+	bookid={bookIndex} />
+  </div>
+</div>
     );
 
 }
