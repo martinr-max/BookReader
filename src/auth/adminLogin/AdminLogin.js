8fedfcbb3927ed90416e10af0917a36fd4e98c9f
martinr-max <74423661+martinr-max@users.noreply.github.com>
2020-11-25 20:52:24 +0200

    Update AdminLogin.js

--- src/auth/adminLogin/AdminLogin.js
+++ src/auth/adminLogin/AdminLogin.js
@@ -12,31 +12,34 @@ import CircularIndeterminate from '../../shared/progressCircle/ProgressCircle';
 
 export default function AdminLogin() {
 
-	const { register, handleSubmit } = useForm();
-	const [error, setError] = useState("");
-	const [isLoading, setIsloading] = useState(false);
+  const {
+    register,
+    handleSubmit
+  } = useForm();
+  const [error, setError] = useState("");
+  const [isLoading, setIsloading] = useState(false);
 
-	const history = useHistory();
-	const auth = useContext(AuthContext);
+  const history = useHistory();
+  const auth = useContext(AuthContext);
 
-	const onSubmit = async (data) => {
-		let user = data;
-		try {
-			setIsloading(true);
-			const responseData = await axios.post('http://localhost:8000/api/admin/login', user);
-			const userId = responseData.data.id;
-			//const token = responseData.data.accessToken;
-			const role = responseData.data.roles;
-			auth.login(userId, role[0].id);
-			history.push('/admin/adminpanel');
-		} catch (err) {
-			setError(err.response.data.message);
-		}
+  const onSubmit = async (data) => {
+    let user = data;
+    try {
+      setIsloading(true);
+      const responseData = await axios.post('http://localhost:8000/api/admin/login', user);
+      const userId = responseData.data.id;
+      //const token = responseData.data.accessToken;
+      const role = responseData.data.roles;
+      auth.login(userId, role[0].id);
+      history.push('/admin/adminpanel');
+    } catch (err) {
+      setError(err.response.data.message);
+    }
 
-	}
+  }
 
-	return (
-		<React.Fragment>
+  return (
+    <React.Fragment>
       { error &&  <Alert severity="error"> { error } </Alert> }
       {isLoading && !error && <CircularIndeterminate size="7rem" color="primary" />}
         <Container> 
@@ -68,5 +71,5 @@ export default function AdminLogin() {
           </form>
         </Container>
       </React.Fragment>
-	);
+  );
 }
